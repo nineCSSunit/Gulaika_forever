9d183fa99c5dd799bfe86c91b9f3fb08f3050afc
Артём <NoWishesEver@yandex.ru>
2024-10-03 07:44:54 +0300

    Aplpa 2.0 - Добавил обработку наличия/отсутсвия Москвы

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 import sys
 import asyncio
 import logging
-import black
 import time
 
 
@@ -17,6 +16,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -25,23 +25,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
-
-
 #####################################################       Считывание конфигурационного файла      ####################
 
 
-with open('config.json') as config_file:
+with open("config.json") as config_file:
     config = json.load(config_file)
 
-TOKEN = config['TELEGRAM_TOKEN']
-API_KEY = config['API_KEY']
+TOKEN = config["TELEGRAM_TOKEN"]
+API_KEY = config["API_KEY"]
 
 
 ######################################################      Ботоводство     ############################################
-
-
-
 
 
 # Инициализация бота и диспетчера с памятью состояний
@@ -70,7 +64,9 @@ async def enter_start_point(message: Message, state: FSMContext):
     await state.update_data(start_point=message.text)
 
     # Спрашиваем про промежуточные точки
-    await message.answer("Введите промежуточные точки (по одной за раз). Когда закончите, напишите 'Стоп'.")
+    await message.answer(
+        "Введите промежуточные точки (по одной за раз). Когда закончите, напишите 'Стоп'."
+    )
     await state.set_state(PointForm.intermediate_points)
 
 
@@ -85,7 +81,9 @@ async def enter_intermediate_points(message: Message, state: FSMContext):
     await state.update_data(intermediate_points=intermediate_points)
 
     # Продолжаем диалог
-    await message.answer("Введите ещё одну промежуточную точку или напишите 'Стоп' для завершения.")
+    await message.answer(
+        "Введите ещё одну промежуточную точку или напишите 'Стоп' для завершения."
+    )
 
 
 # Обработчик, когда пользователь вводит "Стоп"
@@ -109,9 +107,11 @@ async def enter_end_point(message: Message, state: FSMContext):
     end_point = data.get("end_point")
 
     # Формируем и выводим список всех точек
-    points = [f"Начальная: {start_point}"] + \
-             [f"Промежуточная: {point}" for point in intermediate_points] + \
-             [f"Конечная: {end_point}"]
+    points = (
+        [f"Начальная: {start_point}"]
+        + [f"Промежуточная: {point}" for point in intermediate_points]
+        + [f"Конечная: {end_point}"]
+    )
 
     await message.answer("\n".join(points))
 
@@ -127,7 +127,6 @@ async def enter_end_point(message: Message, state: FSMContext):
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
 
 
 async def main() -> None:
@@ -146,11 +145,10 @@ def get_good_route(link):
 
         # Настройки для headless режима (запуск без GUI)
         chrome_options = Options()
-        #chrome_options.add_argument("--headless")  # Запуск в headless режиме
-        #chrome_options.add_argument("--window-size=1920x1080")
-        #chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
-        #chrome_options.add_argument("--disable-gpu")  # отключаем GPU для headless режима
-
+        # chrome_options.add_argument("--headless")  # Запуск в headless режиме
+        # chrome_options.add_argument("--window-size=1920x1080")
+        # chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
+        # chrome_options.add_argument("--disable-gpu")  # отключаем GPU для headless режима
 
         # Путь к драйверу
         path_to_driver = r"C:\Users\abrac\Desktop\chromedriver-win64\chromedriver.exe"
@@ -161,19 +159,22 @@ def get_good_route(link):
         # Инициализация драйвера
         driver = webdriver.Chrome(service=service, options=chrome_options)
 
-
         # Открываем страницу
         driver.get(link)
         # Сохраняем первоначальный URL
         initial_url = driver.current_url
-
 
         time.sleep(2)
         # Ищем кнопку и кликаем по ней
         # Ожидание появления кнопки
         try:
             button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[11]/div/div[1]/div[1]/div[1]/div/div[1]/div/div/div[1]/form/div[3]/div[2]'))
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        "/html/body/div[1]/div[2]/div[11]/div/div[1]/div[1]/div[1]/div/div[1]/div/div/div[1]/form/div[3]/div[2]",
+                    )
+                )
             )
 
             # Клик по кнопке
@@ -183,7 +184,9 @@ def get_good_route(link):
             # Ожидание появления кнопки
 
             button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '/html/body/div[7]/div[2]/div/div[2]/button'))
+                EC.element_to_be_clickable(
+                    (By.XPATH, "/html/body/div[7]/div[2]/div/div[2]/button")
+                )
             )
 
             # Клик по кнопке
@@ -192,7 +195,7 @@ def get_good_route(link):
             print(e)
         current_url = driver.current_url
 
-        for i in range(20):
+        for i in range(30):
             if initial_url != driver.current_url:
                 current_url = driver.current_url
                 print(f"Измененная ссылка: {current_url}")
@@ -211,10 +214,20 @@ def get_good_route(link):
 
 def get_cords(location_name):
     location_cords = None
+
+    if "москва" in location_name.lower():
+        location_name = location_name.replace("Москва", "").replace("москва", "").strip()
+        location_name = "Москва, " + location_name
+        print("location_name     ",location_name)
+    if not("москва" in location_name.lower()):
+        location_name = "Москва, " + location_name
+        print("location_name     ", location_name)
+
     try:
         # Запросы к API для получения координат места
         response_location = requests.get(
-            f'https://catalog.api.2gis.com/3.0/items/geocode?q={location_name}&fields=items.point&key={API_KEY}')
+            f"https://catalog.api.2gis.com/3.0/items/geocode?q={location_name}&fields=items.point&key={API_KEY}"
+        )
 
         # Преобразование ответа в JSON
         response_location = response_location.json()
@@ -230,7 +243,7 @@ def get_cords(location_name):
         if location_item and "point" in location_item:
             location_cords = [
                 float(location_item["point"]["lat"]),
-                float(location_item["point"]["lon"])
+                float(location_item["point"]["lon"]),
             ]
         else:
             raise ValueError(f"Не удалось найти координаты для {location_name}")
@@ -240,7 +253,6 @@ def get_cords(location_name):
     except (IndexError, KeyError, ValueError) as e:
         print(f"Ошибка получения координат для {location_name}: {e}")
         return None
-
 
 
 def make_link(places_cords):
@@ -255,18 +267,31 @@ def make_link(places_cords):
     return link
 
 
+
 def search_for_place(input_text):
+    # Обработка input_text
+
+    if "москва" in input_text.lower():
+        input_text = input_text.replace("Москва", "").replace("москва", "").strip()
+        input_text = "Москва, " + input_text
+        print("input_text     ", input_text)
+    if not("москва" in input_text.lower()):
+        input_text = "Москва, " + input_text
+        print("input_text",    input_text)
+
     # Запросы к API
     response_place = requests.get(
-        f'https://catalog.api.2gis.com/3.0/items?q={input_text}&location=37.620262,55.761734&radius=1000&key={API_KEY}')
+        f"https://catalog.api.2gis.com/3.0/items?q={input_text}&key={API_KEY}"
+    )
 
     # Преобразование ответа в JSON
     response_place = response_place.json()
 
     # Извлечение координат
     try:
-        if response_place['result']['items']:  # Проверяем, есть ли элементы в списке
-            address_name = response_place['result']['items'][0]['address_name']
+        if response_place["result"]["items"]:  # Проверяем, есть ли элементы в списке
+            address_name = response_place["result"]["items"][0]["address_name"]
+            print(address_name)
         else:
             print("No items found.")
 
@@ -278,8 +303,12 @@ def search_for_place(input_text):
 
 
 def generate_map_link(places):
+    places_adress = []
     places_cords = []
-
+    for adress in places:
+        adress = search_for_place(adress)
+        if adress:
+            places_adress.append(adress)
     # Получаем координаты для каждого места
     for place in places:
         cords = get_cords(place)
@@ -291,7 +320,6 @@ def generate_map_link(places):
     else:
         print("Не удалось получить координаты для всех мест.")
         return None
-
 
 
 if __name__ == "__main__":
